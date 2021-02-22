d4a83a2c5972060f79ebc8537b590d47b0dcb139
vishalliverpool <42957382+vishalliverpool@users.noreply.github.com>
2021-02-22 13:43:41 +0530

    Create S3.tf

---
+++ S3.tf
@@ -0,0 +1,28 @@
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "terraform-up-and-running-state"
+  # Enable versioning so we can see the full revision history of our
+  # state files
+  versioning {
+    enabled = true
+  }
+  # Enable server-side encryption by default
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "terraform-up-and-running-state"
+    key            = "global/s3/terraform.tfstate"
+    region         = "us-east-2"
+    # Replace this with your DynamoDB table name!
+    #dynamodb_table = "terraform-up-and-running-locks"
+    #encrypt        = true
+  }
+}
