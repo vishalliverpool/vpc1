077e6e9ccd12245e470a855ff6c2b97bfff1f3f3
vishalliverpool <42957382+vishalliverpool@users.noreply.github.com>
2021-02-22 13:51:34 +0530

    Update S3.tf

--- S3.tf
+++ S3.tf
@@ -1,19 +1,19 @@
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "terraform-up-and-running-state-4"
+#resource "aws_s3_bucket" "terraform_state" {
+#  bucket = "terraform-up-and-running-state-4"
   # Enable versioning so we can see the full revision history of our
   # state files
-  versioning {
-    enabled = true
+ # versioning {
+ #   enabled = true
   }
   # Enable server-side encryption by default
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-}
+  #server_side_encryption_configuration {
+   # rule {
+    #  apply_server_side_encryption_by_default {
+    #    sse_algorithm = "AES256"
+     # }
+    #}
+  #}
+#}
 
 terraform {
   backend "s3" {
